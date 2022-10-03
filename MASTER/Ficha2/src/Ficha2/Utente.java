5904a0ea987d2053adc0b4636326bc6d5311faea
pedrobraga <pedrobraga@localhost>
2022-10-03 14:49:28 +0100

    versão 2

--- MASTER/Ficha2/src/Ficha2/Utente.java
+++ MASTER/Ficha2/src/Ficha2/Utente.java
@@ -17,3 +17,33 @@ public class Utente
 		this.peso = peso;
 		imc = 0;
 	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getGenero() {
+		return genero;
+	}
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	public float getAltura() {
+		return altura;
+	}
+	public void setAltura(float altura) {
+		this.altura = altura;
+	}
+	public int getPeso() {
+		return peso;
+	}
+	public void setPeso(int peso) {
+		this.peso = peso;
+	}
